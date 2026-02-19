ef55ae830faf58e35425e79bd0faaed65e4e975a
Anyang Ateny <anyangateny@outlook.com>
2026-02-19 17:06:18 +1030

    feat: implemented server being able to handle multiple connections

--- internal/backend_server/backendserver.go
+++ internal/backend_server/backendserver.go
@@ -10,34 +10,33 @@ import (
 )
 
 type BackendServer struct {
-	ServerNumber int
+	serverNumber int
 	logger       *slog.Logger
 	listener     net.Listener
 }
 
-func NewBackendServer(num int) *BackendServer {
-	return &BackendServer{
-		ServerNumber: num,
-		logger:       slog.Default(),
+func NewBackendServer(num int) (*BackendServer, error) {
+	ln, err := net.Listen("tcp", ":0")
+	if err != nil {
+		return nil, err
 	}
+
+	return &BackendServer{
+		serverNumber: num,
+		logger:       slog.Default(),
+		listener:     ln,
+	}, nil
 }
 
-func (b *BackendServer) ListenAndServe() {
-	var err error
-	b.listener, err = net.Listen("tcp", ":8080")
-	if err != nil {
-		b.logger.Error("Listen Error", "error", err, "server", b.ServerNumber)
-		return
-	}
-
+func (b *BackendServer) AcceptConnections() {
 	for {
 		conn, err := b.listener.Accept()
 		if err != nil {
 			if errors.Is(err, net.ErrClosed) {
-				b.logger.Info("Listener closed, stopping server", "server", b.ServerNumber)
+				b.logger.Info("Listener closed, stopping server", "server", b.serverNumber)
 				return
 			}
-			b.logger.Error("Listener closed", "error", err, "server", b.ServerNumber)
+			b.logger.Error("Error while accepting", "error", err, "server", b.serverNumber)
 			return
 		}
 		go b.handleConnection(conn)
@@ -52,7 +51,7 @@ func (b *BackendServer) handleConnection(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	message, err := reader.ReadString('\n')
 	if err != nil {
-		b.logger.Error("Read Error", "error", err, "server", b.ServerNumber)
+		b.logger.Error("Read Error", "error", err, "server", b.serverNumber)
 		return
 	}
 
@@ -60,7 +59,7 @@ func (b *BackendServer) handleConnection(conn net.Conn) {
 	response := fmt.Sprintf("ACK: %s\n", ackMsg)
 	_, err = conn.Write([]byte(response))
 	if err != nil {
-		b.logger.Error("Server Write Error", "error", err, "server", b.ServerNumber)
+		b.logger.Error("Server Write Error", "error", err, "server", b.serverNumber)
 	}
 }
 
@@ -69,4 +68,11 @@ func (b *BackendServer) Close() error {
 		return b.listener.Close()
 	}
 	return nil
+}
+
+func (b *BackendServer) Addr() net.Addr {
+	if b.listener == nil {
+		return nil
+	}
+	return b.listener.Addr()
 }
